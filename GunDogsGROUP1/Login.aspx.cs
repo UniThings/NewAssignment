d5a7f0cb8051106a29427049663b58f7a3b9cdd1
Nick <whynh001@mymail.unisa.edu.au>
2014-05-28 15:44:23 +0930

    new assignment

---
+++ GunDogsGROUP1/Login.aspx.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment2013SP5
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ GunDogsGROUP1/Site.Master.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GunDogs
+{
+    public partial class SiteMaster : System.Web.UI.MasterPage
+    {
+        private void Page_Init(object sender, EventArgs e)
+        {
+            // this line sets the Session Username value to the current logged in user Username
+            // do not remove or modify else your SQL Statements involving Username WILL NOT WORK
+            Session["Username"] = HttpContext.Current.User.Identity.Name;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
