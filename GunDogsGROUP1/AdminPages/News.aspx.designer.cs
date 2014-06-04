c8475e0fc44390b9e1a0dc9cb39edd7ef65040ac
sonali10 <sonalichawla10@gmail.com>
2014-06-04 16:30:41 +0930

    Revert "Sonali"
    This reverts commit f896ce2c6fcbbd99433392f2cba0aaee1a448a8d.

--- GunDogsGROUP1/AdminPages/News.aspx.designer.cs
+++ GunDogsGROUP1/AdminPages/News.aspx.designer.cs
@@ -11,32 +11,5 @@ namespace GunDogs.AdminPages {
     
     
     public partial class News {
-        
-        /// <summary>
-        /// SDSNews control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SDSNews;
-        
-        /// <summary>
-        /// GridView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
-        
-        /// <summary>
-        /// FormView1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.FormView FormView1;
     }
 }
